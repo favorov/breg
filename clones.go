918cd0eb4235f52e7e74cc9e9d037d2d886bf109
Alexander Favorov <favorov@sensi.org>
2018-03-22 03:38:39 +0300

    better clone record read code. sample info added

--- clones.go
+++ clones.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"regexp"
 )
 
 type clone struct {
@@ -21,7 +22,8 @@ type clone struct {
 	DEnd int
 	JStart int
 	count int64
-	freq float32
+	freq float64
+	sample string
 //actual order in file:
 //count	freq	cdr3nt	cdr3aa	v	d	j	VEnd	DStart	DEnd	JStart
 }
@@ -32,7 +34,14 @@ func readclones(filename string) []clone {
 			log.Fatalf("Error opening file: %v", err)
 	}
 	defer f.Close()
-	
+
+	regstr:="S[0-9]*"
+	sampleregexp,reerr := regexp.Compile(regstr)
+	if reerr != nil {
+			log.Fatalf("Error compiling regexp %v: %v",regstr,reerr)
+	}
+	sample:=sampleregexp.FindString(filename)
+
 	var clones []clone
 
 	rdr := csv.NewReader(bufio.NewReader(f))
@@ -46,34 +55,35 @@ func readclones(filename string) []clone {
 			}
 			log.Fatal(err)
 		}
-		count, _ := strconv.ParseInt(record[0],10,64)
-		freq, _ := strconv.ParseFloat(record[1],32)
-		VEnd, _ := strconv.ParseInt(record[7],10,32)
-		DStart, _ := strconv.ParseInt(record[8],10,32)
-		DEnd, _ := strconv.ParseInt(record[9],10,32)
-		JStart, _ := strconv.ParseInt(record[10],10,32)
-
-		clones = append(clones, clone{
-					count: count,
-					freq: float32(freq),
-					cdr3nt: record[2],
-					cdr3aa: record[3],
-					v: record[4],
-					d: record[5],
-					j: record[6],
-					VEnd: int(VEnd),
-					DStart: int(DStart),
-					DEnd: int(DEnd),
-					JStart: int(JStart),
-    })
+		
+		var record_clone clone
+		record_clone.count, _ = strconv.ParseInt(record[0],10,64)
+		record_clone.freq, _ = strconv.ParseFloat(record[1],32)
+		record_clone.cdr3nt = record[2]
+		record_clone.cdr3aa = record[3]
+		record_clone.v = record[4]
+		record_clone.d = record[5]
+		record_clone.j = record[6]
+		record_clone.VEnd, _ = strconv.Atoi(record[7])
+		record_clone.DStart, _ = strconv.Atoi(record[8])
+		record_clone.DEnd, _ = strconv.Atoi(record[9])
+		record_clone.JStart, _ = strconv.Atoi(record[10])
+		record_clone.sample = sample
+		clones = append(clones, record_clone)
 	}
 	return clones
 }
 
 func main() {
 	S22 := readclones("vdj_.S22_clones.txt")
-	S22 := readclones("vdj_.S22_clones.txt")
-	S22 := readclones("vdj_.S22_clones.txt")
+	println(S22[0].sample)
+//	S23 := readclones("vdj_.S23_clones.txt")
+//	S24 := readclones("vdj_.S24_clones.txt")
+//	S25 := readclones("vdj_.S25_clones.txt")
+//	S26 := readclones("vdj_.S26_clones.txt")
+//	S27 := readclones("vdj_.S27_clones.txt")
+//	S28 := readclones("vdj_.S28_clones.txt")
+//	S29 := readclones("vdj_.S29_clones.txt")
 }
 
 
