581564d9febe3ad8d82049b04de5948bd2c5037f
Alexander Favorov <favorov@sensi.org>
2018-10-11 18:04:02 -0400

    ~ does not work in -jar option, use /home/user

--- scripts/align_and_export_mixcr_clones.sh
+++ scripts/align_and_export_mixcr_clones.sh
@@ -1,9 +1,10 @@
+#! /bin/bash
 #we run it from the folder where the clone files will appear as result
 
-path_to_mixcr="~/gabibov/tools/mixcr-2.1.12"
-path_to_vdjtools="~/gabibov/tools/vdjtools-1.1.11"
+path_to_mixcr="/home/sasha/gabibov/tools/mixcr-2.1.12"
+path_to_vdjtools="/home/sasha/gabibov/tools/vdjtools-1.1.10"
 mixcr="java -jar ${path_to_mixcr}/mixcr.jar"
-vdjtools="java -jar ${path_to_vdjtools}/vdjtools-1.1.11.jar"
+vdjtools="java -jar ${path_to_vdjtools}/vdjtools-1.1.10.jar"
 fastq_path="../fastq"
 
 SAMPLES=(S1 S2 S3 S4 S5 S6 S7 S8 S22 S23 S24 S25 S26 S27 S28 S29)
@@ -18,20 +19,16 @@ for sample in ${SAMPLES[@]}; do
 		vdjca_file=${sample}.${chain}.vdjca
 		if [ ! -f $vdjca_file ]
 		then 
+			echo $mixcr align -c ${chain} -r ${sample}.${chain}.aln.report.txt ${fastq_pair} $vdjca_file
 			$mixcr align -c ${chain} -r ${sample}.${chain}.aln.report.txt ${fastq_pair} $vdjca_file
 		else
 			echo "File $vdjca_file exists."
 		fi
-		#vdjca_file_with_reads=${sample}.${chain}.with.reads.vdjca
-		#if [ ! -f $vdjca_file_with_reads ]
-		#then 
-		#	$mixcr align -g -c ${chain} -r ${sample}.${chain}.aln.report.with.reads.txt ${fastq_pair} $vdjca_file_with_reads
-		#else
-		#	echo "File $vdjca_file_with_reads exists."
-		#fi
+		
 		clns_file=${sample}.${chain}.clns
 		if [ ! -f $clns_file ]
 		then 
+			echo $mixcr assemble -r ${sample}.${chain}.assemb.report.txt $vdjca_file $clns_file
 			$mixcr assemble -r ${sample}.${chain}.assemb.report.txt $vdjca_file $clns_file
 		else
 			echo "File $clns_file exists."
@@ -39,6 +36,7 @@ for sample in ${SAMPLES[@]}; do
 		clones_text_file=${sample}.${chain}.clones.txt
 		if [ ! -f $clones_text_file ]
 		then 
+			echo $mixcr exportClones $clns_file $clones_text_file
 			$mixcr exportClones $clns_file $clones_text_file
 		else
 			echo "File $clones_text_file exists."
@@ -46,6 +44,7 @@ for sample in ${SAMPLES[@]}; do
 		vdj_clones_text_file=vdj.${sample}.${chain}.clones.txt
 		if [ ! -f $vdj_clones_text_file ]
 		then 
+			echo $vdjtools Convert -S mixcr $clones_text_file vdj
 			$vdjtools Convert -S mixcr $clones_text_file vdj
 		else
 			echo "File $vdj_clones_text_file exists."
