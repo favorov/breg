edd4747be075e538def297999c71d2882cd915a6
Alexander Favorov <favorov@sensi.org>
2018-10-12 02:26:32 -0400

    combine clones for 3 chains

---
+++ scripts/combine-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CHAINS=(IGH IGL IGK)
+SHARES=(0 0.05)
+for chain in ${CHAINS[@]}; do
+	for share in ${SiHARES[@]}; do
+		echo ./combine_clones --chain ${chain} --mismatches-share ${share} --clones-folder ../clones/
+		./combine_clones --chain ${chain} --mismatches-share ${share} --clones-folder ../clones/
+		echo done...
+	done
+done
