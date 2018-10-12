7fea708d5cfa34b9a338cc6a2b6f992aa2aac034
Alexander Favorov <favorov@sensi.org>
2018-10-12 02:31:14 -0400

    typo

--- scripts/combine-all.sh
+++ scripts/combine-all.sh
@@ -2,7 +2,7 @@
 CHAINS=(IGH IGL IGK)
 SHARES=(0 0.05)
 for chain in ${CHAINS[@]}; do
-	for share in ${SiHARES[@]}; do
+	for share in ${SHARES[@]}; do
 		echo ./combine_clones --chain ${chain} --mismatches-share ${share} --clones-folder ../clones/
 		./combine_clones --chain ${chain} --mismatches-share ${share} --clones-folder ../clones/
 		echo done...
