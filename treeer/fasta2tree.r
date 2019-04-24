4f9dd0e32702d8f281c41c7509eae8d6711d27e0
Alexander Favorov <favorov@sensi.org>
2019-04-24 13:46:07 +0300

    built nj tree on hamming distances with some norm

--- treeer/fasta2tree.r
+++ treeer/fasta2tree.r
@@ -1,11 +1,16 @@
 library(ape)
 library(phangorn)
 
-plottree<-function(fastaname,pdfname){
-clone<-read.FASTA(fastaname)
-cloneDat<-phyDat(clone)
-pdf(pdfname)
-plot(nj(dist.ml(cloneDat)),main=fastaname,direction = "rightwards")
-#edgelabels(t$edge.length, bg="black", col="white", font=2)
-dev.off()
+plottree<-function(fastaname,pdfname=NA){
+  if (is.na(pdfname))
+    pdfname<-paste0(gsub("\\..*$", "",fastaname),".pdf")
+  clone<-read.FASTA(fastaname)
+  cloneDat<-phyDat(clone)
+  tree<-nj(dist.hamming(cloneDat))
+  pdf(pdfname)
+  plot(tree,main=fastaname,direction = "rightwards")
+  edgelabels(tree$edge.length, bg="black", col="white", font=.5)
+  dev.off()
 }
+
+sapply(list.files(pattern="*.fasta_aln"),plottree)
